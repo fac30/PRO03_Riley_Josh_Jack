866e46b60f34c66ad0fb6b4732ccdaaa2a4b017f
rilez366 <rilez366@outlook.com>
2024-09-25 11:43:05 +0100

    open-ai

--- dist/Api-handling/openAI-handling.js
+++ dist/Api-handling/openAI-handling.js
@@ -1,1 +1,17 @@
 "use strict";
+const OpenAI = require('openai');
+const dotenv = require('dotenv');
+// Load environment variables from the .env file
+dotenv.config();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY || "";
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY, // Get the API key from the environment variable
+});
+async function main() {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "Give me a fun fact about Lithuania" }],
+        model: "gpt-4o",
+    });
+    console.log(completion.choices[0].message.content);
+}
+main();
--- src/database-handling/get-country.ts
+++ src/database-handling/get-country.ts
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
+
 const dotenv = require("dotenv");
 dotenv.config();
-
 const supabaseURL: string = process.env.SUPABASE_URL ?? "";
+
 const supabaseKey: string = process.env.SUPABASE_KEY ?? "";
 // console.log(supabaseURL, supabaseKey);
 
@@ -10,10 +11,17 @@ const supabase = createClient(supabaseURL, supabaseKey);
 
 const getCountry = async () => {
   const { data } = await supabase.from("countries").select();
-  //   console.log(data);
 
-  //   console.log(data[0].country);
-  return data[0].country;
+  const countryObject = {
+    country: data[0].country,
+    code: data[0].code,
+  };
+
+  console.log(countryObject);
+
+  return countryObject;
 };
 
 module.exports = getCountry;
+
+export {};
