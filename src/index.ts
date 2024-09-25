fe7635bf20f6da7d3428ddb01b7702c21dbe6b74
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-09-25 11:08:17 +0100

    delete index.ts code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-const getCountry = require("./database-handling/get-country");
 
-// Call the getCountry function
-const asyncGetCountry = async () => {
-  await getCountry();
-};
-
-asyncGetCountry();
-
-// console.log("I am in index.ts");
