a171a0adf7ec2de27558ccbf322e340314fa5270
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-09 12:32:51 +0100

    moved secret functionality to it's own file rather than in get country function

--- src/aws/aws-secret.ts
+++ src/aws/aws-secret.ts
@@ -3,6 +3,9 @@ const {
   GetSecretValueCommand,
 } = require("@aws-sdk/client-secrets-manager");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const secret_name = "server_secret";
 
 const client = new SecretsManagerClient({
@@ -11,23 +14,35 @@ const client = new SecretsManagerClient({
 
 // Define a function to get the secret
 async function getSecret() {
-  let response;
+  let supabaseURL: string;
+  let supabaseKey: string;
+  let openAIKey: string;
+  if (process.env.USE_AWS_SECRETS === "true") {
+    let response;
 
-  try {
-    response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-    );
+    try {
+      response = await client.send(
+        new GetSecretValueCommand({
+          SecretId: secret_name,
+          VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        })
+      );
 
-    // Parse the SecretString to get the actual secret
-    const secret = JSON.parse(response.SecretString);
-    return secret; // Return the secret
-  } catch (error) {
-    console.error("Error retrieving secret:", error);
-    throw error; // Re-throw to handle it later
+      // Parse the SecretString to get the actual secret
+      const secret = JSON.parse(response.SecretString);
+      supabaseURL = secret.SUPABASE_URL; // Access the URL from the secret
+      supabaseKey = secret.SUPABASE_KEY; // Access the key from the secret
+      openAIKey = secret.OPENAI_API_KEY;
+    } catch (error) {
+      console.error("Error retrieving secret:", error);
+      throw error; // Re-throw to handle it later
+    }
+  } else {
+    supabaseURL = process.env.SUPABASE_URL || "supabseurlmissing";
+    supabaseKey = process.env.SUPABASE_KEY || "supabasekeymissing";
+    openAIKey = process.env.OPENAI_API_KEY || "openaikeymissing";
   }
+  return { supabaseURL, supabaseKey, openAIKey };
 }
 
 module.exports = { getSecret };
--- src/database-handling/get-country.ts
+++ src/database-handling/get-country.ts
@@ -1,32 +1,16 @@
 const { getSecret } = require("../aws/aws-secret");
-const { supabaseKeyDotenv, supabaseURLDotenv } = require("../dotenv/dotenv");
 const { dbRequest } = require("./db-request");
-
-let supabaseURL: string;
-
-let supabaseKey: string;
 
 // Use the secret in an async IIFE
 const getCountries = async () => {
-  if (process.env.USE_AWS_SECRETS === "true") {
-    try {
-      const secret = await getSecret(); // Call the function to retrieve the secret
-      supabaseURL = secret.SUPABASE_URL; // Access the URL from the secret
-      supabaseKey = secret.SUPABASE_KEY; // Access the key from the secret
-
-      // Initialize Supabase client after the secrets are retrieved
-      const myData = await dbRequest(supabaseURL, supabaseKey);
-      console.log(myData);
-      return myData;
-    } catch (error) {
-      console.error("Error using secret:", error);
-    }
-  } else {
-    supabaseURL = supabaseURLDotenv;
-    supabaseKey = supabaseKeyDotenv;
+  try {
+    // Initialize Supabase client after the secrets are retrieved
+    const { supabaseURL, supabaseKey } = await getSecret();
     const myData = await dbRequest(supabaseURL, supabaseKey);
     console.log(myData);
     return myData;
+  } catch (error) {
+    console.error("Error using secret:", error);
   }
 };
 
