22d52d707c2c002eb3a699513a6d4cc37bd1954a
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-09 12:52:18 +0100

    remove comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,12 @@ app.use(cors());
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
-// Initialize a variable to store all countries
 let allCountries: any[] = [];
 
 // Immediately invoked function to fetch countries when the server starts
 (async () => {
   try {
-    allCountries = await getCountries(); // Fetch the countries from the database
+    allCountries = await getCountries();
   } catch (error) {
     console.error("Error fetching countries:", error);
   }
@@ -33,7 +32,6 @@ app.get("/countries", async (req: any, res: any) => {
 
 app.post("/random-fact", getRandomFact);
 
-// Start the server and listen on the specified port
 app.listen(PORT, () => {
   let serverType: string = "Unknown";
   if (process.env.USE_AWS_SECRETS === "true") {
@@ -41,7 +39,7 @@ app.listen(PORT, () => {
   } else if (process.env.USE_AWS_SECRETS === "false") {
     serverType = "Dotenv";
   }
-  console.log(`Server is running on port ${PORT}: Server type --${serverType}`); // Log that the server has started successfully
+  console.log(`Server is running on port ${PORT}: Server type --${serverType}`);
 });
 
 export {};
